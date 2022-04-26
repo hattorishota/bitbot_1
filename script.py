da1f01b168ea4de0f159c9071a6bf4e876efe94d
hattorishota <hattorishota.eng@gmail.com>
2022-04-27 02:15:01 +0900

    no message

---
+++ script.py
@@ -0,0 +1,27 @@
+import time
+# time.sleep(3)
+
+mylist = ['apple', 'banana', 'peach']
+print(mylist[0] + ',' + mylist[1] + ',' + mylist[2])
+for val in mylist:
+  print(val)
+
+s = 'aaa' 'bbb'
+print(s)
+
+# 改行を無視する方法
+a = 'aaabbbccc'\
+    'dddeeefff'\
+    'ggghhhiii'\
+
+print('a = ' + a)
+
+b = ('aaabbbccc'
+    'dddeeefff'
+    'ggghhhiii')
+
+print('b = ' + b)
+
+u = time.gmtime()
+print(u)
+print(u[0])
